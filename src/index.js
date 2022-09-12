6d9f2c263f213ffeefa0b429b7ff87be36ef894f
Saaim-K <saaimkhan221@gmail.com>
2022-09-12 20:33:40 +0500

    Resume-React

--- src/index.js
+++ src/index.js
@@ -1,17 +1,35 @@
+
+
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+import './index.css'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function Hi() {
+  return <div className='body'>
+    <h1 className='heading'>RESUME</h1>
+    <p className='name'><strong>Saaim Ahmed Khan</strong></p><hr />
+    <div className='info'>
+      <p>House # B-16 Naveed Bangalows Gulistan-e-Johar, Karachi</p>
+      <p>0312-1092078</p>
+      <p><a href="#" className='email' >saaimkhan221@gmail.com</a></p><br></br>
+    </div>
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <div className='objective'>
+      <p className='obj'><strong>CAREER OBJECTIVE</strong></p><hr />
+      <p className='para'>Dedicated and Determined Undergrad student seeking a position that<br></br>
+        encourages hard work, organization and communication skills.</p><br></br>
+    </div>
+
+    <div className='per-info'>
+      <p className='obj'><strong>PERSONAL INFORMATION</strong></p><hr />
+      <p className='para'><strong>Date of Birth: </strong>09<sup>th</sup> Oct 2000</p>
+      <p className='para'><strong>Marital Status: </strong>Single</p>
+      <p className='para'><strong>Religion: </strong>Islam</p>
+      <p className='para'><strong>Nationality: </strong>Pakistani</p>
+
+    </div>
+
+  </div>
+}
+
+ReactDOM.render(<Hi />, document.querySelector('#root'));
